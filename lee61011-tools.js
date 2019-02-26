b01feaf39dfe2a50d3d2223c7d7ffff5fd9ef1a5
lee61011 <lee61011@126.com>
2019-02-26 22:05:52 +0800

    myQueryURLParameter()

--- lee61011-tools.js
+++ lee61011-tools.js
@@ -6,3 +6,21 @@
 String.prototype.myTrim = function myTrim() {
     return this.replace(/^\s+|\s+$/g, '');
 };
+
+
+
+/* 
+    这个方法可以快速将 url 传参值进行解析
+    @ url.myQueryURLParameter()
+    @ 返回一个对象，包含 url 中所有参数的键值对
+*/
+String.prototype.myQueryURLParameter = function myQueryURLParameter() {
+    var obj = {};
+    this.replace(/([^?&=#]+)=([^?&=#]+)/g, function () {
+        obj[arguments[1]] = arguments[2];
+    });
+    this.replace(/#([^?=&#]+)/g, function () {
+        obj['HASH'] = arguments[1];
+    });
+    return obj;
+};
