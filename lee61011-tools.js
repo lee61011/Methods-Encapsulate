81b7bc362f624520d9f34a77e15a47c3a0673acf
lee61011 <lee61011@126.com>
2019-03-07 22:09:07 +0800

    myFormatTime

--- lee61011-tools.js
+++ lee61011-tools.js
@@ -24,3 +24,20 @@ String.prototype.myQueryURLParameter = function myQueryURLParameter() {
     });
     return obj;
 };
+
+
+
+/* 
+    通过 myFormatTime() 这个方法可以来格式化时间字符串 
+    @ 可以根据自己设置的模板来解析  myFormatTime('{1}-{2} {3}:{4}')、myFormatTime('{3}:{4}:{5}')……
+*/
+String.prototype.myFormatTime = function myFormatTime() {
+    var ary = this.match(/\d+/g),
+        template = arguments[0] || '{0}年{1}月{2}日 {3}时{4}分{5}秒';	//	如果传递自己的格式模板，就按传递的来解析，否则按照默认的模板解析
+    template = template.replace(/\{(\d+)\}/g, function () {
+        var value = ary[arguments[1]] || '0';
+        value.length < 2 ? value = '0' + value : null;
+        return value;
+    });
+    return template;
+};
